77a9f5e0cfeaeaa8c698ce7e492b3d52d9217355
alvansga <zralvansga@gmail.com>
2024-11-13 23:33:13 +0700

    add heartbeat blink

---
+++ blink.py
@@ -0,0 +1,15 @@
+from machine import Pin
+
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.on()
+    sleep(0.1)
+    led.off()
+    sleep(0.08)
+    led.on()
+    sleep(0.1)
+    led.off()
+    sleep(0.72)
